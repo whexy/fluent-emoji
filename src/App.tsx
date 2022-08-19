addf146c5390f9b8eedbc3af6c6bb88f7f4cb04c
whexy <25165025+whexy@users.noreply.github.com>
2022-08-20 04:26:45 +0800

    feat: share

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const assets = import.meta.glob('./assets/**/*.svg', {
   as: 'url',
@@ -59,11 +60,44 @@ function App() {
     }
   }
 
-  const [eyes, setEyes] = useState<number>(0);
-  const [head, setHead] = useState<number>(0);
-  const [eyebrows, setEyebrows] = useState<number>(0);
-  const [mouth, setMouth] = useState<number>(0);
-  const [details, setDetails] = useState<number>(0);
+  const [eyes, setEyes] = useState<number>(-1);
+  const [head, setHead] = useState<number>(-1);
+  const [eyebrows, setEyebrows] = useState<number>(-1);
+  const [mouth, setMouth] = useState<number>(-1);
+  const [details, setDetails] = useState<number>(-1);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const getInt = (s: string) => parseInt(params.get(s) || '-1');
+
+    setEyes(getInt('eyes'));
+    setHead(getInt('head'));
+    setEyebrows(getInt('eyebrows'));
+    setMouth(getInt('mouth'));
+    setDetails(getInt('details'));
+    draw();
+  }, []);
+
+  const draw = () => {
+    var canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    const drawElement = (idx: number, gallery: string[]) => {
+      if (idx >= 0) {
+        var image = new Image();
+        image.src = gallery[idx];
+        image.onload = () => {
+          canvas.getContext('2d')?.drawImage(image, 0, 0, 144, 144);
+        };
+      }
+    };
+    canvas.getContext('2d')?.clearRect(0, 0, canvas.width, canvas.height);
+    drawElement(head, HeadSVGs);
+    drawElement(eyes, EyeSVGs);
+    drawElement(eyebrows, EyeBrowsSVGs);
+    drawElement(mouth, MouthSVGs);
+    drawElement(details, DetailsSVGs);
+  };
+
+  useEffect(() => draw(), [eyes, head, eyebrows, mouth, details]);
 
   const goWild = () => {
     const getRandom = (min: number, max: number) => {
@@ -79,7 +113,7 @@ function App() {
   };
 
   return (
-    <div className="flex justify-center gap-4 bg-gray-100 p-4">
+    <div className="flex items-start justify-center gap-4 bg-gray-100 p-4">
       <div className="max-w-3xl">
         <h1 className="pt-8 text-center text-3xl font-bold">
           Fluent Emoji Maker
@@ -114,45 +148,24 @@ function App() {
           selected={details}
           setSelected={setDetails}
         />
-        <div className="">
-          <button
-            className="rounded-lg bg-white px-4 py-2"
-            onClick={() => goWild()}>
-            Go Wild
-          </button>
-        </div>
       </div>
-      <div className="sticky top-28 h-36 w-36 flex-shrink-0 rounded-lg bg-white">
-        {head >= 0 && (
-          <img
-            src={HeadSVGs[head]}
-            className="absolute top-0 right-0 left-0 bottom-0 m-auto h-20 w-20"
-          />
-        )}
-        {eyes >= 0 && (
-          <img
-            src={EyeSVGs[eyes]}
-            className="absolute top-0 right-0 left-0 bottom-0 m-auto h-20 w-20"
-          />
-        )}
-        {eyebrows >= 0 && (
-          <img
-            src={EyeBrowsSVGs[eyebrows]}
-            className="absolute top-0 right-0 left-0 bottom-0 m-auto h-20 w-20"
-          />
-        )}
-        {mouth >= 0 && (
-          <img
-            src={MouthSVGs[mouth]}
-            className="absolute top-0 right-0 left-0 bottom-0 m-auto h-20 w-20"
-          />
-        )}
-        {details >= 0 && (
-          <img
-            src={DetailsSVGs[details]}
-            className="absolute top-0 right-0 left-0 bottom-0 m-auto h-20 w-20"
-          />
-        )}
+      <div className="sticky top-28 ">
+        <div className="flex-shrink-0 rounded-lg bg-white">
+          <canvas id="canvas" height={144} width={144} />
+        </div>
+        <div className="flex flex-col space-y-2">
+          <button
+            className="mt-4 rounded-lg bg-white px-4 py-2"
+            onClick={() => goWild()}>
+            🤪 Go Wild
+          </button>
+          <CopyToClipboard
+            text={`${window.location.origin}?eyes=${eyes}&head=${head}&eyebrows=${eyebrows}&mouth=${mouth}&details=${details}`}>
+            <button className="mt-4 rounded-lg bg-white px-4 py-2">
+              👯‍♀️ Share
+            </button>
+          </CopyToClipboard>
+        </div>
       </div>
     </div>
   );
